d5c39d7de9281aed4a9c2551dd9806fbf553b168
Slothy <william.alex.davis@gmail.com>
2023-08-08 09:35:32 -0600

    connected api to test front end

--- gptFuncs.js
+++ gptFuncs.js
@@ -1,14 +1,13 @@
-const OpenAIApi = require('openai');
+const {OpenAIApi} = require('openai');
 
 const openai = new OpenAIApi({
   api_key: 'YOUR-API-KEY-HERE'
 });
 
-async function getChatCompletion(prompt, chatContext) {
-  const response = await openai.chatCompletion({
+async function getChatCompletion(prompt) {
+  const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
-      ...chatContext,
       { role: 'user', content: prompt }
     ]
   });
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const getChatCompletion = require('./gptFuncs.js');
 const app = express();
-const port = 3000;
+const path = require('path');
+const port = 5001;
 
 // server static react files
-app.use(express.static.path.join(__dirname, 'dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// serve app
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    }
+);
+
+// handle post requests
+app.post('/getCompletion', async (req, res) => {
+    console.log(req.body);
+    const prompt = req.body;
+    const response = await getChatCompletion(prompt);
+    console.log(response);
+    res.send(response);
+    }
+);
+
+// start server
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+    }
+);
